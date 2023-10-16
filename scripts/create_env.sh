c7195bb236de8ad14fae5402e62a5fcc011bab79
albilaga <albilaga@pm.me>
2023-10-16 23:20:24 +0700

    test

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Read each line in .env.sample file
-while IFS= read -r line
-do
-  # Check if line isn't empty or a comment
-  if [[ ! -z "$line" && ! "$line" =~ ^# ]]; then
-    # remove any existing instances of the variable in .env
-    sed -i "/^$line/d" .env
-    # append variable to .env
-    echo "$line" >> .env
+# Read the env.sample file
+while IFS= read -r line; do
+  # Extract the secret name from the placeholder, assuming placeholders are like {{SECRET_NAME}}
+  secret_name="${line#*{{}"
+  secret_name="${secret_name%%}}*}"
+
+  if [[ -n "$secret_name" ]]; then
+    # Replace the placeholder with the corresponding GitHub secret
+    secret_value="${{ secrets.$secret_name }}"
+    line="${line/\{\{${secret_name}\}\}/$secret_value}"
   fi
-done < ".env.sample"
+
+  echo "$line"
+done < .env.sample > .env
