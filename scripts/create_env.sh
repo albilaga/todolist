fa931c9f38d19d4028c276838c10e3d4ab58509e
albilaga <albilaga@pm.me>
2023-10-16 23:44:49 +0700

    Fix Scripts

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-# Read the env.sample file
-while IFS= read -r line; do
-  # Extract the secret name from the placeholder, assuming placeholders are like {{SECRET_NAME}}
-  secret_name="${line#*{{}"
-  secret_name="${secret_name%%}}*}"
+# Function to get the value from GitHub secrets or use the default
+get_secret_value() {
+  local secret_name="$1"
+  local default_value="$2"
 
-  if [[ -n "$secret_name" ]]; then
-    # Replace the placeholder with the corresponding GitHub secret
-    secret_value="${{ secrets.$secret_name }}"
-    line="${line/\{\{${secret_name}\}\}/$secret_value}"
+  if [[ -n "${!secret_name}" ]]; then
+    echo "${!secret_name}"
+  else
+    echo "$default_value"
   fi
+}
 
-  echo "$line"
+# Read .env.sample and create .env
+while IFS= read -r line; do
+  # Check if the line contains a key-value pair
+  if [[ "$line" =~ ^[A-Za-z_][A-Za-z_0-9]*= ]]; then
+    # Extract key and value
+    key="${line%%=*}"
+    value="${line#*=}"
+
+    secret_value=$(get_secret_value "key" "$value")
+
+    # Write to .env
+    echo "$key=$secret_value"
+  else
+    # Pass-through non-key-value lines
+    echo "$line"
+  fi
 done < .env.sample > .env
