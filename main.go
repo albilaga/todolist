c74793eb5194a89a9c20f638d7fd4e34d71fce2d
albilaga <albilaga@pm.me>
2023-11-15 23:28:14 +0700

    remove otel

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"github.com/riandyrn/otelchi"
 	"log/slog"
 	"net/http"
 	"os"
@@ -31,14 +29,14 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
-	serviceName := "todolist"
-	otelShutdown, err := setupOtelSDK(ctx, serviceName, "0.0.1")
-	if err != nil {
-		return
-	}
-	defer func() {
-		err = errors.Join(err, otelShutdown(context.Background()))
-	}()
+	//serviceName := "todolist"
+	//otelShutdown, err := setupOtelSDK(ctx, serviceName, "0.0.1")
+	//if err != nil {
+	//	return
+	//}
+	//defer func() {
+	//	err = errors.Join(err, otelShutdown(context.Background()))
+	//}()
 
 	dsn := fmt.Sprintf("postgresql://%v:%v@%v:%v/%v?sslmode=%v", dbConfig.Username, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name, dbConfig.SslMode)
 	conn, err := pgxpool.New(ctx, dsn)
@@ -57,7 +55,7 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Use(otelchi.Middleware(serviceName, otelchi.WithChiRoutes(r)))
+	//r.Use(otelchi.Middleware(serviceName, otelchi.WithChiRoutes(r)))
 
 	r.Route("/todos", func(r chi.Router) {
 		r.Get("/", listTodos)
